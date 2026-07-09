e792cb76eb965bc8819702ec450a0f8c33303a88
StepanovMaksim <130572592+StepanovMaksim@users.noreply.github.com>
2026-07-09 20:54:00 +0300

    Effect

--- Assets/Scripts/ResponseCounter.cs
+++ Assets/Scripts/ResponseCounter.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 
 // ������� ������� ������
 public class ResponseCounter : MonoBehaviour
-    
-{  
-    [SerializeField] private GameObject[]  _finishHeartImages;
-    [SerializeField] private GameObject[]  _finishStarsImages;
-    [SerializeField] private GameObject[] _starImages; 
+
+{
+    [SerializeField] private GameObject[] _finishHeartImages;
+    [SerializeField] private GameObject[] _finishStarsImages;
+    [SerializeField] private GameObject[] _starImages;
     [SerializeField] private GameObject[] _heartImages;
+
     [SerializeField] TextMeshProUGUI textCountFalse;
     [SerializeField] TextMeshProUGUI _textCount;
+    [Header("Эффекты")]
+    [SerializeField] GameObject _effectTrue;
+
     int _count = 0;
     int _countFalse = 0;
     public int TrueChoice()
@@ -23,35 +27,37 @@ public class ResponseCounter : MonoBehaviour
         return _countFalse;
     }
     public void CountFalse()
-    
-    {      ActiveHearts();
-        
+    {
+        ActiveHearts();
+
         _countFalse = _countFalse + 1;
-        
+
         textCountFalse.text = _countFalse.ToString();
-        
     }
     public void PlusCount()
     {
         ActiveStars();
         _count = _count + 1;
         _textCount.text = _count.ToString();
+        _effectTrue.SetActive(true);
     }
 
     void ActiveStars()
     {
         _finishStarsImages[_count].SetActive(true);
         _starImages[_count].SetActive(true);
-        
+
     }
 
     void ActiveHearts()
     {
-        _finishHeartImages[_countFalse].SetActive(false);
-        _heartImages[_countFalse].SetActive(false);
-    }     
-       
+        if (_countFalse < _finishHeartImages.Length)
+        {
+            _finishHeartImages[_countFalse].SetActive(false);
+            _heartImages[_countFalse].SetActive(false);
+        }
+    }
+
 }
 
 
-   
