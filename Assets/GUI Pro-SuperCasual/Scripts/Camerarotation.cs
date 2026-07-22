e042d7c23398c2f26f6ec19ff221c60f4f43e6c6
liman706 <hrundelek2020@gmail.com>
2026-07-22 19:00:45 +0300

    f
    f

---
+++ Assets/GUI Pro-SuperCasual/Scripts/Camerarotation.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Camerarotation : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 4f;
+    float xRotation = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+        xRotation -= mouseY; 
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.Rotate(Vector3.up * mouseX);
+        Camera.main.transform.localEulerAngles = new Vector3(xRotation, 0f, 0f);
+    }
+}
---
+++ Assets/GUI Pro-SuperCasual/Scripts/PlayerMove.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController _controller;
+    [SerializeField] float _speed;
+    [SerializeField] float _gravitationalAcceleration = 9.81f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(x, 0, z);
+        _controller.Move(movement * _speed * Time.deltaTime);
+    }
+}
