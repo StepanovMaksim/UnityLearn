9b4d3b4821e891c402250581e2f6e3c3948ca85a
liman706 <hrundelek2020@gmail.com>
2026-07-24 19:07:24 +0300

    g
    g

--- Assets/GUI Pro-SuperCasual/Scripts/Camerarotation.cs
+++ Assets/GUI Pro-SuperCasual/Scripts/Camerarotation.cs
@@ -7,7 +7,8 @@ public class Camerarotation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
--- Assets/GUI Pro-SuperCasual/Scripts/PlayerMove.cs
+++ Assets/GUI Pro-SuperCasual/Scripts/PlayerMove.cs
@@ -4,7 +4,10 @@ public class PlayerMove : MonoBehaviour
 {
     CharacterController _controller;
     [SerializeField] float _speed;
-    [SerializeField] float _gravitationalAcceleration = 9.81f;
+    [SerializeField] float _gravity = 9.81f;
+    [SerializeField] float _jumpHeight = 3f;
+    private bool _isGrounded;
+    Vector3 _velocity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,13 +18,21 @@ public class PlayerMove : MonoBehaviour
     
     void Update()
     {
+        _isGrounded = _controller.isGrounded;
+        if (_isGrounded && _velocity.y < 0)
+            _velocity.y = -2f;
+        if (Input.GetKeyDown(KeyCode.Space)&& _isGrounded)
+                                          _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+        _velocity.y -= _gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime); 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(x, 0, z);
+        Vector3 movement = transform.right * x + transform.forward * z;
         _controller.Move(movement * _speed * Time.deltaTime);
         if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
             _speed *=3 ;
         if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
             _speed /= 3 ;
+        
     }
 }
