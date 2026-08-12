223ba29287c37df535d70cd8c477e59439883823
liman706 <hrundelek2020@gmail.com>
2026-08-12 17:48:24 +0300

    а
    а

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -2,24 +2,32 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
+    
     [SerializeField] GameObject _bullet;
     [SerializeField] Transform _bulletSpawn;
 
-    [SerializeField] private float _fireRate = 0.1f; 
-     
+    [SerializeField] private float _fireRate = 0.1f;
+    private float _nextFire;
 
     void Start()
     {
-        
+        _nextFire = _fireRate;
     }
 
     void Update()
     {
         
-        if (Input.GetMouseButton(0) && 0 >= _fireRate)
+        if (Input.GetMouseButton(0))
         {
-            if 
-            Instantiate(_bullet, _bulletSpawn.position, _bulletSpawn.rotation);
+            
+            if (_fireRate > 0)
+                _fireRate -= Time.deltaTime;
+            else
+            {
+                 Instantiate(_bullet, _bulletSpawn.position, _bulletSpawn.rotation);
+                 _fireRate = _nextFire;
+            }
+           
            
         }
     }
