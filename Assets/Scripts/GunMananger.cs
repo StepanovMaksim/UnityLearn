7caf17734148ece45bf86b1139202da139e3917d
liman706 <hrundelek2020@gmail.com>
2026-08-20 17:33:59 +0300

    а
    а

--- Assets/Scripts/GunMananger.cs
+++ Assets/Scripts/GunMananger.cs
@@ -4,6 +4,9 @@ public class GunMananger : MonoBehaviour
 {
     [SerializeField] private GameObject _Gun1;
     [SerializeField] private GameObject _Gun2;
+    [SerializeField]  Camera _camera;
+    [SerializeField] float _cameraDistance;
+    [SerializeField]  Transform _aimTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,14 +18,27 @@ public class GunMananger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             _Gun2.SetActive(true);
-            _Gun1.SetActive(false); 
+            _Gun1.SetActive(false);
         }
+
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             _Gun2.SetActive(false);
             _Gun1.SetActive(true);
-        }   
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            _camera.fieldOfView = _cameraDistance;
             
-    }
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+             _camera.fieldOfView = 60f;
+        }
+           
+    
+}
 }
