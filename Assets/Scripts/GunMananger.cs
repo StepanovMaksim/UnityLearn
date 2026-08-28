a7a19f77c74ac291830b9813a683bfdc3f390c98
liman706 <hrundelek2020@gmail.com>
2026-08-28 16:48:40 +0300

    f
    f

--- Assets/Scripts/GunMananger.cs
+++ Assets/Scripts/GunMananger.cs
@@ -1,60 +1,80 @@
-using UnityEngine; 
+using UnityEngine;
 
-public class GunMananger : MonoBehaviour 
-{ 
-    [SerializeField] private GameObject[] _guns; 
-    [SerializeField] Camera _camera; 
-    [SerializeField] Transform _aimTransform; 
-    [SerializeField] float _cameraDistance; 
-    int _gunIndex; //    
+public class GunMananger : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _guns;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Transform _aimTransform;
+    [SerializeField] private float _cameraDistance = 40f; 
+    [SerializeField] private float _smoothSpeed = 10f;    
 
-    private void Start() 
-    { 
-        ActiveGun(0); 
-    } 
+    private int _gunIndex;
+    private Vector3[] _defaultPositions;
+    private bool _isAiming = false; 
+    private float _defaultFOV = 60f;
 
-    // Update is called once per frame 
-    void Update() 
-    { 
-        if (Input.GetKeyDown(KeyCode.Alpha1)) 
-        { 
-            ActiveGun(0); 
-            _camera.fieldOfView = 60f; 
-        } 
-        if (Input.GetKeyDown(KeyCode.Alpha2)) 
-        { 
-            ActiveGun(1); 
-            _camera.fieldOfView = 60f; 
-        } 
-        if (Input.GetKeyDown(KeyCode.Alpha3)) 
-        { 
-            ActiveGun(2); 
-            _camera.fieldOfView = 60f; 
-        } 
-        if (Input.GetMouseButtonDown(1)) 
-        { 
-            _camera.fieldOfView = _cameraDistance; 
-            _guns[_gunIndex].transform.position = _aimTransform.position; 
-        } 
-        if (Input.GetMouseButtonUp(1)) 
-        { 
-            _camera.fieldOfView = 60f; 
-        } 
-    } 
+    private void Start()
+    {
+        _defaultPositions = new Vector3[_guns.Length];
+        for (int i = 0; i < _guns.Length; i++)
+        {
+            _defaultPositions[i] = _guns[i].transform.localPosition;
+        }
 
-    void ActiveGun(int index) 
-    { 
-        _gunIndex = index; 
-        for (int i = 0; i < _guns.Length; i++) 
-        { 
-            if (_gunIndex == i) 
-            { 
-                _guns[i].SetActive(true); 
-            } 
-            else 
-            { 
-                _guns[i].SetActive(false); 
-            } 
-        } 
-    } 
+        ActiveGun(0);
+    }
+
+    void Update()
+    {
+        // Смена оружия на клавиши 1, 2, 3
+        if (Input.GetKeyDown(KeyCode.Alpha1)) ActiveGun(0);
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) ActiveGun(1);
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) ActiveGun(2);
+
+        // Проверяем нажатие и отпускание правой кнопки мыши
+        if (Input.GetMouseButtonDown(1))
+        {
+            _isAiming = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            _isAiming = false;
+        }
+
+        // Плавное управление прицеливанием
+        AnimateAiming();
+    }
+
+    private void AnimateAiming()
+    {
+        float targetFOV = _isAiming ? _cameraDistance : _defaultFOV;
+        
+        Vector3 targetWeaponPos = _isAiming 
+            ? _guns[_gunIndex].transform.parent.InverseTransformPoint(_aimTransform.position) 
+            : _defaultPositions[_gunIndex];
+        
+        _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, targetFOV, Time.deltaTime * _smoothSpeed);
+        
+        _guns[_gunIndex].transform.localPosition = Vector3.Lerp(
+            _guns[_gunIndex].transform.localPosition, 
+            targetWeaponPos, 
+            Time.deltaTime * _smoothSpeed
+        );
+    }
+
+    void ActiveGun(int index)
+    {
+        if (_guns != null && _guns[_gunIndex].activeSelf)
+        {
+            _guns[_gunIndex].transform.localPosition = _defaultPositions[_gunIndex];
+        }
+
+        _gunIndex = index;
+        for (int i = 0; i < _guns.Length; i++)
+        {
+            _guns[i].SetActive(i == _gunIndex);
+        }
+        
+        _isAiming = false; 
+    }
 }
